f4d83515d38bd80523e7efbad746d3c437589c74
smkiran <sm.kiran996@gmail.com>
2017-02-13 21:30:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone={
+    title: 'article-one Im kiran',
+    heading:'article-one',
+    date:'feb 1 2017',
+    content:`<p>
+                this content for my first article. this content for my first article. this content for my first article.
+            </p>
+            <p>
+                this content for my first article. this content for my first article. this content for my first article.
+            </p>
+            <p>
+                this content for my first article. this content for my first article. this content for my first article.
+            </p> `
+};
+
+function create (data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmltemplet=` 
+   <html>
+    <head>
+        <title>
+         Article one I'm Kiran    
+        </title>
+            <meta name="viewpoint" contents="width=device-width,initial=scale-1" />
+     <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+       <div>
+           <a href="/">HOME</a>
+        </div>
+        <hr/>
+        <h3> 
+          ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+          ${contents}
+        </div>    
+        </div>
+    </body>
+</html>`;
+return htmltemplet;
+}
+
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ res.send(create(article-one));
 });
 
 app.get('/article-one',function(req,res){

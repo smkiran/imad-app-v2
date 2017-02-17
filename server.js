493c56200508a605dd6975777dae33cb57abd89f
smkiran <sm.kiran996@gmail.com>
2017-02-17 19:32:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter',function(req,res){
-    counter=counter+1;
+var counter = 0;
+app.get('/counter',function(req, res){
+    counter=counter + 1;
     res.send(counter.toString());
 });
 

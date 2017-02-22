7cc1f151f25d37865d949ddbadc78fb13aae1052
smkiran <sm.kiran996@gmail.com>
2017-02-22 21:28:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,18 @@ var counter = 0;
 
 
 button.onclick = function(){
+    
+   var request =new XMLHttpRequest();
    
-    counter =counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
-    
+   request.onreadystatechange =function(){
+       if( request.readystate == XMLHttpRequest.DONE ){
+        if(request.status == 200){      
+          counter =counter + 1;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+        }
+       }
+   };
+   request.open('GET','http://http://smkiran.imad.hasura-app.io/counter',true);
+   request.send(null);
 };

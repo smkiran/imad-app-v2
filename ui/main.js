80ea74f7b6ea3ab9218d4d129c8cddc30b7a4085
smkiran <sm.kiran996@gmail.com>
2017-02-22 21:34:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,6 @@ button.onclick = function(){
         }
        }
    };
-   request.open('GET','http://http://smkiran.imad.hasura-app.io/counter',true);
+   request.open('GET','http://smkiran.imad.hasura-app.io/counter',true);
    request.send(null);
 };
